7a5904ffde98b9c05c9721188c993cb17e08adec
bc30138 <sasori.axele@gmail.com>
2021-09-16 17:07:20 +0300

    web share button

---
+++ shweb/static/scripts/release.js
@@ -0,0 +1,19 @@
+function openLyrics(id) {
+    document.getElementById(id).classList.toggle("show");
+}
+
+function copyToClipboard(text) {
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(text);
+
+    var el = document.querySelector('.copied-alarm')
+
+    el.style.transition = "opacity 0.2s linear"
+    el.style.visibility = "visible"
+    el.style.opacity = 1;
+    setTimeout(function () {
+        el.style.visibility = "hidden"
+        el.style.opacity = 0
+        el.style.transition = "visibility 0s 0.2s, opacity 0.2s linear"
+    }, 1000);
+}

f57c45aca43b1ef662c7cf0d201649f371821cfc
bc30138 <sasori.axele@gmail.com>
2021-09-17 16:17:29 +0300

    share button mobile

---
+++ shweb/static/scripts/mobile/release.js
@@ -0,0 +1,13 @@
+var share_btn = document.querySelector(".share")
+
+share_btn.addEventListener("click", share)
+
+function openLyrics(id) {
+    document.getElementById(id).classList.toggle("show");
+}
+
+function share() {
+    navigator.share({
+        url: text,
+    })
+}

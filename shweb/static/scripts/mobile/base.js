9177d3168ecb78ef154a76ab1df17399e5c45d64
bc30138 <sasori.axele@gmail.com>
2021-09-18 15:02:44 +0300

    babel mobile

--- shweb/static/scripts/mobile/base.js
+++ shweb/static/scripts/mobile/base.js
@@ -2,8 +2,10 @@ var menu = document.querySelector(".menu")
 var ham = document.querySelector(".ham")
 var xIcon = document.querySelector(".xIcon")
 var menuIcon = document.querySelector(".menuIcon")
+var release_links = document.getElementById('discogs')
 
 ham.addEventListener("click", toggleMenu)
+release_links.addEventListener("click", open_release_menu)
 
 function toggleMenu() {
     if (menu.classList.contains("showMenu")) {
@@ -15,4 +17,13 @@ function toggleMenu() {
         xIcon.style.display = "block";
         menuIcon.style.display = "none";
     }
+}
+
+function open_release_menu() {
+    document.getElementById('release-links').classList.toggle("show");
+    document.getElementById("release-expand-icon").classList.toggle("expand-more")
+}
+
+function change_lang(lang) {
+    window.location.href = window.location.href.split('?')[0] + "?lang=" + lang;
 }

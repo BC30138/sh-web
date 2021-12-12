72d19593c753c2a7dda063cfe4385c57a367ea5d
bc30138 <sasori.axele@gmail.com>
2021-12-12 13:46:41 +0300

    postdeploy testt

--- .platform/hooks/postdeploy/01_pybabel.sh
+++ .platform/hooks/postdeploy/01_pybabel.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-pybabel extract -F shweb/babel.cfg -o shweb/messages.pot shweb
-pybabel update -i shweb/messages.pot -d shweb/translations
-pybabel compile -d shweb/translations
+# pybabel extract -F shweb/babel.cfg -o shweb/messages.pot shweb
+# pybabel update -i shweb/messages.pot -d shweb/translations
+# pybabel compile -d shweb/translations
+
+pip3 freeze
